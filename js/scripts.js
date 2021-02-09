d0b313002defc26b8e6b0939c226562daf72c07c
Ankit <ankitsisodiya76@gmail.com>
2021-02-10 00:45:39 +0530

    select modal test

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,24 @@ $(document).ready(function(){
 	$('.brand-col').click(function () {
 		$('#selectbrandmodal').modal("hide");
 		$('#selectcarmodal').modal("show");
-        //alert( $(this).attr('setval') );
+        var brand = $(this).attr('setval') ;
+        $('.car-brand').text(brand);
+    });
+    $('.model-col').click(function () {
+		$('#selectcarmodal').modal("hide");
+        var model = $(this).attr('setval') ;
+        $('.car-model').text(model);
     })
     $('#selectmodalback').click(function(){
     	$('#selectbrandmodal').modal("show");
 		$('#selectcarmodal').modal("hide");
+
     });
 });
   for (var i = 0; i<52; i++){
-    $('#brand-row').append('<div class="col-4 col-sm-3 brand-col  p-2" setval="'+i+ '" ><div class="brands border rounded text-center"><img class=" p-2 img-fluid" src="assets/brands/audi.png"><h6>Audi</h6></div></div>');
+    $('#brand-row').append('<div class="col-4 col-sm-3 brand-col  p-2" setval="Audi" ><div class="brands border rounded text-center"><img class=" p-2 img-fluid" src="assets/brands/audi.png"><h6>Audi</h6></div></div>');
   }
 
   for (var i = 0; i<50; i++){
-    $('#car-row').append('<div class="col-4 col-sm-3 brand-col  p-2" setval="'+i+ '" ><div class="brands border rounded text-center"><img class=" p-2 img-fluid" src="assets/model/Audi_12.png"><h6>Audi</h6></div></div>');
+    $('#car-row').append('<div class="col-4 col-sm-3 model-col  p-2" setval=" Audi Q3" ><div class="models border rounded text-center"><img class=" p-2 img-fluid" src="assets/model/Audi_12.png"><h6>Audi</h6></div></div>');
   }
